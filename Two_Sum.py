2f8cc78eeab409cf8f660168a8724035c79c23ac
Timothy Mihet <71294550+mihet101@users.noreply.github.com>
2023-02-22 16:10:27 -0500

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,34 +1,17 @@
 class Solution(object):
     
     def twoSum(self, nums, target):
-        
-        for i in range(len(nums)):         
-            x = nums[i]
-            y = target - nums[i]
-            if (y == nums[i]):              #check if we have n+n = target
-                if (y in nums[(i+1):]):
-                    y_index = (nums[(i+1):].index(y) + (i+1)) 
-                    x_index = nums.index(x)
-                    return[x_index, y_index]   
-            else:                           #is m+n = target  
-                if (y in nums):
-                    y_index = nums.index(y)
-                    x_index = nums.index(x)
-                    return[x_index, y_index]
+            """
+            :type nums: List[int]
+            :type target: int
+            :rtype: List[int]
+            """
+            prevMap = {}  #create hash map
 
-
-class betterSolution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        prevMap = {}  #create hash map
-        
-        for i, n in enumerate(nums):  #enum the given list with index i and value n
-            diff = target - n
-            if diff in prevMap: #if the difference already exists in the map then return it
-                return[prevMap[diff], i]
-            prevMap[n] = i    #if not then add it to the hash map
-        return
+            for i, n in enumerate(nums):  #enum the given list with index i and value n
+                diff = target - n
+                if diff in prevMap: #if the difference already exists in the map then return it
+                    return[prevMap[diff], i]
+                prevMap[n] = i    #if not then add it to the hash map
+            return
+  
